e8b5d1658eb486299732545cbb6d5e4f6ac20c96
Prodenxo <leo.irak@hotmail.com>
2026-03-02 15:42:48 -0300

    ⬆️ att: Liberação de CORS para permitir acesso de qualquer origem

--- backend/src/config/server.js
+++ backend/src/config/server.js
@@ -3,7 +3,15 @@ const cors = require("cors");
 
 const app = express();
 
-app.use(cors());
+// Liberação de CORS para permitir acesso de qualquer origem
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(`[REQ] ${req.method} ${req.url}`);
